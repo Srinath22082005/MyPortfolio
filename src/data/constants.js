061b49b0a7e7f4f3dba84e90c44417c7920eed22
Srinath22082005 <167328887+Srinath22082005@users.noreply.github.com>
2025-06-01 16:42:20 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -21,7 +21,7 @@ export const Bio = {
     "Passinate Full Stack Developer, AI/ML enthusiast, and aspiring Blockchain Developer with expertise in Java (Spring Boot), React.js, Node.js, Python (for ML/DL), and blockchain technologies like Ethereum and Solidity. Passionate about solving real-world problems through full stack development, AI/ML, and decentralized solutions.",
   github: "https://github.com/Srinath22082005",
   resume:
-    "https://drive.google.com/file/d/1Cu8SMkaiwRT1C-Xt7ijJJSbVcVMv3RY7/view?usp=sharing",
+    "https://drive.google.com/file/d/1-5hrhD8kBVNft30FYVzGe9csifEPVXTm/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/srinath-s-0354082b3/",
   insta: "https://www.instagram.com/mad_mask_01/",
   leetcode:"https://leetcode.com/u/Srinath_Sp1/",
